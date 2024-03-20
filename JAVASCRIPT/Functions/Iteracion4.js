2f8fd86ad00fc10dcde2aade592bc7dae83bf6db
Gisl4 <lopgiz25@gmail.com>
2024-03-20 21:17:53 +0100

    modificacion de functions-iteraciones

---
+++ JAVASCRIPT/Functions/Iteracion4.js
@@ -0,0 +1,16 @@
+// Calcular el promedio
+
+// Calcular un promedio es una tarea extremadamente común. Puedes usar este array para probar tu función:
+
+const numbers = [12, 21, 38, 5, 45, 37, 6];
+
+const average = (array) => {
+  let suma = 0;
+
+  for (i = 0; i < array.length; i++) {
+    suma += array[i];
+  }
+  return suma / array.length;
+};
+
+console.log(average(numbers));
